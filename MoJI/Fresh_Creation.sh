6a4aefa4c8382b389e0fd1702c162b04c3060931
Yoonchulchung <yoonchul005@gmail.com>
2025-07-05 03:51:50 +0900

    removed shell

--- MoJI/Fresh_Creation.sh
+++
@@ -1,9 +0,0 @@
-watchman watch-del-all || true
-sudo rm -rf node_modules android/.gradle android/.cxx \
-       android/app/build android/app/.cxx ~/.gradle
-
-npm install
-
-cd android && ./gradlew clean
-
-npx react-native run-android
