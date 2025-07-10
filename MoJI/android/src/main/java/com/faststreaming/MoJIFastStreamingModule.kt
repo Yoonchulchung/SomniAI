f23d3aa349a88a39307d8a955e6133e2e43228f2
Yoonchulchung <yoonchul005@gmail.com>
2025-07-10 23:16:15 +0900

    Initiated Module and Package

---
+++ MoJI/android/src/main/java/com/faststreaming/MoJIFastStreamingModule.kt
@@ -0,0 +1,7 @@
+package com.moji
+
+import android.util.Log
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.turbomodule.core.interfaces.CallInvokerHolder
+
