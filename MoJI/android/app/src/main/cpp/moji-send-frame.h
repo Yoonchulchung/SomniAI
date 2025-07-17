02a4b3bbd29a7c9953ec2582daf9a7190deb037b
Yoonchulchung <yoonchul005@gmail.com>
2025-07-17 19:44:32 +0900

    Added : curl-cpp

--- MoJI/android/app/src/main/cpp/moji-send-frame.h
+++ MoJI/android/app/src/main/cpp/moji-send-frame.h
@@ -2,6 +2,25 @@
 #define MOJI_SEND_H
 
 #include <jsi/jsi.h>
+#include <android/log.h>
+
+/* ========== UDP Socket Send ======== */
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+/* =================================== */
+
+/* =========== HTTP ==================*/
+#include <curl/curl.h>
+
+#define LOG_TAG "MoJIPlugin"
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
 
 #ifdef __cplusplus
 
@@ -13,6 +32,11 @@ class MoJISend {
     public:
         static jsi::Value MoJISendFrame(jsi::Runtime& runtime, const jsi::Value& thisValue, 
             const jsi::Value* arguments, size_t count);
+
+        static bool sendBufferOverUDP(const uint8_t* bufferData, size_t bufferSize, 
+            const std::string& ip, uint16_t port);
+
+        static bool sendBufferOverHTTP(const uint8_t* bufferData, size_t bufferSize, const std::string& url);
 };
 
 #endif
