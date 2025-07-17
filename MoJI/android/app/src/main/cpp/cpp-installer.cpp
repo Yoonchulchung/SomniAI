7ad3bbd1c8b34c5054fb7d67f6e8a39f2ff6cb2b
Yoonchul Chung <84674889+Yoonchulchung@users.noreply.github.com>
2025-07-17 12:19:04 +0900

    Fixed : line

--- MoJI/android/app/src/main/cpp/cpp-installer.cpp
+++ MoJI/android/app/src/main/cpp/cpp-installer.cpp
@@ -12,12 +12,16 @@ using namespace facebook::react;
 
 extern "C"
 JNIEXPORT void JNICALL
-
-Java_com_moji_faststreaming_MoJIFastStreamingModule_nativeInstall(JNIEnv *env, jobject thiz, jlong jsi_runtime_ref,
+Java_com_moji_faststreaming_MoJIFastStreamingModule_nativeInstall(
+                JNIEnv *env, 
+                jobject thiz, 
+                jlong jsi_runtime_ref,
                 jobject js_call_invoker_holder) {
 
                     auto jsRuntime{ reinterpret_cast<jsi::Runtime*>(jsi_runtime_ref) };
-                    auto jsCallInvoker{ jni::alias_ref<react::CallInvokerHolder::javaobject>{reinterpret_cast<react::CallInvokerHolder::javaobject>(js_call_invoker_holder) }->cthis()->getCallInvoker() };
+                    auto jsCallInvoker{ jni::alias_ref<react::CallInvokerHolder::javaobject>{
+                                        reinterpret_cast<react::CallInvokerHolder::javaobject>(js_call_invoker_holder) }
+                                      ->cthis()->getCallInvoker() };
 
                     MoJIPlugin::installMoJI(*jsRuntime, jsCallInvoker);
                 }
