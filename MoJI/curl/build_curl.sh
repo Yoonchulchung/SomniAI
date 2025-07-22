96da272f63d5557c926475707794e211d75df07e
Yoonchul Chung <84674889+Yoonchulchung@users.noreply.github.com>
2025-07-22 14:23:09 +0900

    Updated : build_curl.sh

--- MoJI/curl/build_curl.sh
+++ MoJI/curl/build_curl.sh
@@ -1,13 +1,30 @@
-
-
 export ANDROID_NDK_HOME=/home/yoonchul/Android/Sdk/ndk/26.1.10909125/
+export OPENSSL_HOME="$(pwd)/openssl-android-install"
 
-cmake -S ./curl -B ../build -G Ninja \
-    -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake \
-    -DANDROID_ABI=arm64-v8a \
-    -DANDROID_PLATFORM=android-21 \
-    -DCURL_STATICLIB=ON \
-    -DBUILD_CURL_EXE=OFF \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DHTTP_ONLY=ON \
-    -OPENSSL_CRYPTO_LIBRARY_OPENSSL_INCLUDE_DIR
+
+cmake -S ./curl -B ./build -G Ninja \
+  -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
+  -DANDROID_ABI=arm64-v8a \
+  -DANDROID_PLATFORM=android-21 \
+  -DBUILD_SHARED_LIBS=OFF -DBUILD_CURL_EXE=OFF -DHTTP_ONLY=ON \
+  -DCURL_USE_OPENSSL=ON \
+  -DOPENSSL_ROOT_DIR="$OPENSSL_HOME" \
+  -DOPENSSL_CRYPTO_LIBRARY="$OPENSSL_HOME/lib/libcrypto.a" \
+  -DOPENSSL_SSL_LIBRARY="$OPENSSL_HOME/lib/libssl.a" \
+  -DOPENSSL_INCLUDE_DIR="$OPENSSL_HOME/include" \
+  -DCURL_BROTLI=OFF \
+  -DCURL_ZSTD=OFF \
+  -DCURL_NGHTTP2=OFF \
+  -DCURL_USE_LIBIDN2=OFF \
+  -DCURL_USE_LIBPSL=OFF
+
+
+echo "\n\n====== CMake Configure/Generate is Completed! ==== \n\n"
+
+ninja -C ./build 
+
+echo "\n\n====== Ninja Build is Completed! ==== \n\n"
+
+sudo cmake --install ./build
+
+echo "\n\n====== Finished Installing! ======== \n\n"
