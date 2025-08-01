dadd89ea09999b1a48766b007001e1b7ea96e49a
yoonchul <yoonchul005@gmail.com>
2025-08-01 19:24:24 +0900

    deleted : unnecessary files

--- mmpose/mmpose/evaluation/metrics/__init__.py
+++ mmpose/mmpose/evaluation/metrics/__init__.py
@@ -1,16 +1,3 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .coco_metric import CocoMetric
-from .coco_wholebody_metric import CocoWholeBodyMetric
-from .hand_metric import InterHandMetric
-from .keypoint_2d_metrics import (AUC, EPE, NME, JhmdbPCKAccuracy,
-                                  MpiiPCKAccuracy, PCKAccuracy)
-from .keypoint_3d_metrics import MPJPE
-from .keypoint_partition_metric import KeypointPartitionMetric
-from .posetrack18_metric import PoseTrack18Metric
-from .simple_keypoint_3d_metrics import SimpleMPJPE
+from .ieee_vip_cup_2021_metric import IEEEVIPCup2021Metric
 
-__all__ = [
-    'CocoMetric', 'PCKAccuracy', 'MpiiPCKAccuracy', 'JhmdbPCKAccuracy', 'AUC',
-    'EPE', 'NME', 'PoseTrack18Metric', 'CocoWholeBodyMetric',
-    'KeypointPartitionMetric', 'MPJPE', 'InterHandMetric', 'SimpleMPJPE'
-]
+__all__ = ['IEEEVIPCup2021Metric']
