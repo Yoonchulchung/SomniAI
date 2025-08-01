d0f672146d6cbd17cffd9135152cdd5990e608e9
yoonchul <yoonchul005@gmail.com>
2025-08-02 00:36:54 +0900

    deleted : unnecessary files

--- mmpose/mmpose/models/pose_estimators/__init__.py
+++ mmpose/mmpose/models/pose_estimators/__init__.py
@@ -1,6 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .bottomup import BottomupPoseEstimator
-from .pose_lifter import PoseLifter
 from .topdown import TopdownPoseEstimator
 
-__all__ = ['TopdownPoseEstimator', 'BottomupPoseEstimator', 'PoseLifter']
+__all__ = ['TopdownPoseEstimator']
