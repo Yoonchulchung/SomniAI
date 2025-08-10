ed32302e5177b85a450f5d60390a9be1ec0471cb
yoonchul <yoonchul005@gmail.com>
2025-08-10 15:05:35 +0900

    updated : log

--- app/fastapi/utils/log.py
+++ app/fastapi/utils/log.py
@@ -1,5 +1,19 @@
+from datetime import datetime
+import logging
 
+logger = logging.getLogger("SomniAI")
+logger.setLevel(logging.INFO)
 
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
 
-def SomniAI_log(message : str):
-    print(f"[SomniAI] {message}")
+formatter = logging.Formatter("[SomniAI %(asctime)s] %(message)s")
+console_handler.setFormatter(formatter)
+
+if not logger.handlers:
+    logger.addHandler(console_handler)
+    
+    
+def SomniAI_log(*message: str):
+    msg = " ".join(str(m) for m in message)
+    logger.info(msg)
