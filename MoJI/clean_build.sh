202d217220e0e04dfd4ad8d47d4e799d0d6da22e
YoonchulChung <yoonchul005@gmail.com>
2025-08-16 23:17:35 +0900

    updated : clean build

--- MoJI/clean_build.sh
+++ MoJI/clean_build.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+clear
+echo "Clean building MoJI Application..."
+MOJI_ROOT="$(pwd)"
+
+CURL_PATH="$(pwd)/curl/curl_output"
+
+if [ ! -d "$CURL_PATH" ] ; then
+    echo "Installing Curl for Android ..."
+    cd "${CURL_PATH%/*}" ; ./build.sh || {
+        echo "[ERROR] Something wrong while installing curl for android!"
+        exit 1
+    }
+fi
+
+cd "$MOJI_ROOT"
+
 watchman watch-del-all || true
 sudo rm -rf node_modules android/.gradle android/.cxx \
        android/app/build android/app/.cxx ~/.gradle
