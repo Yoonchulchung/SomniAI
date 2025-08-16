682c482d81f539d86468a6ea99a465cda5135063
YoonchulChung <yoonchul005@gmail.com>
2025-08-16 23:14:21 +0900

    updated : build

--- MoJI/build.sh
+++ MoJI/build.sh
@@ -1,2 +1,19 @@
-cd android && ./gradlew clean
+#!/bin/bash
+
+echo "Building MoJI Application..."
+MOJI_ROOT="$(pwd)"
+
+CURL_PATH="$(pwd)/curl/curl_output"
+
+if [ ! -d "$CURL_PATH" ] ; then
+    echo "Installing Curl for Android ..."
+    cd "${CURL_PATH%/*}" ; ./build.sh || {
+        echo "[ERROR] Something wrong while installing curl for android!"
+        exit 1
+    }
+fi
+
+cd "$MOJI_ROOT"
+
+cd android ; ./gradlew clean
 npx react-native run-android
