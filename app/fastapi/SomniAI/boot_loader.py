6555938f0d629b444d4d098b09be35a08dbf41be
yoonchul <yoonchul005@gmail.com>
2025-09-05 20:38:08 +0900

    fixed : typo

--- app/fastapi/SomniAI/boot_loader.py
+++ app/fastapi/SomniAI/boot_loader.py
@@ -1,21 +1,25 @@
 from SomniAI.log import SomniAI_log
 from SomniAI.process import ProcessGPU
-from SomniAI.AI import ModelLoader
+from SomniAI.AI import GPUModelLoader
 from SomniAI.AI.registry import vlm_register, vision_register
 from SomniAI.AI import Inference
 from SomniAI.AI import Dataset
 from SomniAI.registry import get_cfg
 
 def bootstrap() -> ProcessGPU:
+    # We build everything in bootstrap
     SomniAI_log("=" * 10, " SomniAI FastAPI Server ", "=" * 10)
 
     SomniAI_cfg = get_cfg()
-    gpu = ProcessGPU(SomniAI_cfg.AI, Inference, Dataset, SomniAI_log)
+    gpu = ProcessGPU(SomniAI_cfg.AI, 
+                     Inference(SomniAI_cfg.AI.PROMPT, SomniAI_cfg.AI.QUESTION), 
+                     Dataset(SomniAI_cfg.AI), 
+                     SomniAI_log)
     
-    model_loader = ModelLoader(SomniAI_cfg.AI.FREE_MEM_THRESHOLD,
+    model_loader = GPUModelLoader(SomniAI_cfg.AI.FREE_MEM_THRESHOLD,
                                vlm_register=vlm_register,
                                vision_register=vision_register, 
-                               logger =SomniAI_log)
+                               logger=SomniAI_log)
     
     model = model_loader.get_model(SomniAI_cfg.AI.MODEL_NAME, 0)
     gpu.add_model(model)
