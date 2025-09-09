6b807c087171316cd4e87a514b99e2c58d02bd18
Seonghoye <yessho9917@gmail.com>
2025-09-09 13:55:29 +0900

    Update ring.go

--- app/go/queue/ring.go
+++ app/go/queue/ring.go
@@ -8,60 +8,57 @@ import (
 type Ring struct {
 	head     int
 	tail     int
-	size     int
-	data     []string
-	cap     int
+	capacity int
+	items    []string
 	mu       sync.Mutex
 	notFull  *sync.Cond
 	notEmpty *sync.Cond
 }
 
-func New(cap int) *Ring {
+func NewRing(capacity int) *Ring {
 	r := &Ring{
-		data:     make([]string, cap),
-		cap: cap,
+		items:    make([]string, capacity),
+		capacity: capacity,
 	}
 	r.notFull = sync.NewCond(&r.mu)
 	r.notEmpty = sync.NewCond(&r.mu)
 	return r
 }
 
-//  ENQUEUE 함수
 func (r *Ring) Enqueue(ctx context.Context, item string) error {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	for r.size == r.cap {
-		
-		if ctx.Err() != nil {
+	for (r.tail+1)%r.capacity == r.head {
+		select {
+		case <-ctx.Done():
 			return ctx.Err()
+		default:
+			r.notFull.Wait()
 		}
-		r.notFull.Wait()
 	}
 
-	r.data[r.tail] = item
-	r.tail = (r.tail + 1) % r.cap
-	r.size++
+	r.items[r.tail] = item
+	r.tail = (r.tail + 1) % r.capacity
 	r.notEmpty.Signal()
 	return nil
 }
 
-//  DEQUEUE 함수
 func (r *Ring) Dequeue(ctx context.Context) (string, error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	for r.size == 0 {
-		
-		if ctx.Err() != nil {
+	for r.head == r.tail {
+		select {
+		case <-ctx.Done():
 			return "", ctx.Err()
+		default:
+			r.notEmpty.Wait()
 		}
-		r.notEmpty.Wait()
 	}
 
-	item := r.data[r.head]
-	r.head = (r.head + 1) % r.cap
-	r.size--
+	item := r.items[r.head]
+	r.head = (r.head + 1) % r.capacity
 	r.notFull.Signal()
 	return item, nil
 }
