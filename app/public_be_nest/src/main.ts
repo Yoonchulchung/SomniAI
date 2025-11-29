c81ad98d0260e6588c1b8e735a41e129a6bab418
Yoonchulchung <yoonchul005@gmail.com>
2025-11-29 20:22:54 +0900

    fix : typo

--- app/public_be_nest/src/main.ts
+++ app/public_be_nest/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4000',
+    origin: ['http://localhost:4000', 'http://192.168.0.39:4000'],
     credentials: true,
   });
   
